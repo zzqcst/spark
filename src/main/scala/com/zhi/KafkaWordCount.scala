b51c87bd41d2abd274308bd3f890b4de27f9457e
zhi <zzqlrr@gmail.com>
2018-09-26 20:11:44 +0800

    sync

--- src/main/scala/com/zhi/KafkaWordCount.scala
+++ src/main/scala/com/zhi/KafkaWordCount.scala
@@ -14,13 +14,13 @@ object KafkaWordCount {
         val group = "1" //topic所在的group，可以设置为自己想要的名称，比如不用1，而是val group = "test-consumer-group"
         val topics = "wordsender" //topics的名称
         val numThreads = 1 //每个topic的分区数
-        val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
-        val lineMap = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap)
+        val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap//(topic,1)
+        val lineMap = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap)//(主题，消息)
         val lines = lineMap.map(_._2)
         val words = lines.flatMap(_.split(" "))
         val pair = words.map(x => (x, 1))
         val wordCounts = pair.reduceByKeyAndWindow(_ + _, _ - _, Minutes(2), Seconds(10), 2) //这行代码的含义在下一节的窗口转换操作中会有介绍
-        wordCounts.print
+        wordCounts.print()
         ssc.start
         ssc.awaitTermination
     }
